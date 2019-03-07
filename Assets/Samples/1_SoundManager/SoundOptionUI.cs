97f4a594f5a119ee01c83d4a430e0e35b217b931
ahyeong2533 <ahyeong2533@gmail.com>
2019-03-08 01:58:11 +0900

    SoundManager 샘플 구현
    볼륨 조절/뮤트
    씬 이동시 매니저 이동

---
+++ Assets/Samples/1_SoundManager/SoundOptionUI.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundOptionUI : MonoBehaviour
+{
+    public Slider bgmSlider;
+    public Slider sfxSlider;
+
+    public Toggle bgmToggle;
+    public Toggle sfxToggle;
+
+    private void OnEnable()
+    {
+        bgmSlider.value = SoundManager.Instance.BgmVolume;
+        sfxSlider.value = SoundManager.Instance.SfxVolume;
+
+        bgmToggle.isOn = SoundManager.Instance.BgmMute;
+        sfxToggle.isOn = SoundManager.Instance.SfxMute;
+    }
+
+    public void SetBgmVolume(float value)
+    {
+        SoundManager.Instance.BgmVolume = value;
+    }
+
+    public void SetSfxVolume(float value)
+    {
+        SoundManager.Instance.SfxVolume = value;
+    }
+
+    public void SetBgmMute(bool flag)
+    {
+        SoundManager.Instance.BgmMute = flag;
+    }
+
+    public void SetSfxMute(bool flag)
+    {
+        SoundManager.Instance.SfxMute = flag;
+    }
+}
