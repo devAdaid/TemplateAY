2a00ac3afeabe8a508ad96918f0bcd9c1917b815
ahyeong2533 <ahyeong2533@gmail.com>
2019-03-08 01:21:22 +0900

    싱글턴 구현 및 SoundManager 적용
    싱글턴 - Mono, Persistent 두 종류
    prefab/instantiate 등으로 싱글턴 생성 가능
    SoundManager 구현 및 프리팹 생성

---
+++ Assets/Scripts/Test/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Test : MonoBehaviour
+{
+    public UnityEvent events;
+
+    private void Start()
+    {
+        events.Invoke();
+    }
+
+    public void SoundTest()
+    {
+        SoundManager.Instance.Echo();
+    }
+}
